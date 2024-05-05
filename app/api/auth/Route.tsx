8628aad5da570a7a828145139e6620347e8dee35
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-05-05 23:25:39 +0530

    Added Post Route

--- app/api/auth/Route.tsx
+++ app/api/auth/Route.tsx
@@ -3,3 +3,7 @@ import { NextRequest } from "next/server";
 export function GET(req: NextRequest) {
     return Response.json({msg:"Get Route"})
 }
+
+export function POST(req: NextRequest) {
+    return Response.json({ msg: "Post Route" })
+}
