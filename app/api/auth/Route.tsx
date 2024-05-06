b818906ebe83ad8fa7c20ac6cf1c9faf9a4fda68
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-05-06 15:39:30 +0530

    Changed Stack From NEXT to MERN

--- app/api/auth/Route.tsx
+++
@@ -1,9 +0,0 @@
-import { NextRequest } from "next/server";
-
-export function GET(req: NextRequest) {
-    return Response.json({msg:"Get Route"})
-}
-
-export function POST(req: NextRequest) {
-    return Response.json({ msg: "Post Route" })
-}
