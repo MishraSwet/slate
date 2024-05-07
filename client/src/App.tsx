a601dd948c74e455b6c6c763694cd0b623b4680a
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-05-07 23:58:35 +0530

    Added Canvas Element to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 export default function App() {
-  return <div className="bg-blue-500">
-    App
+  return <div className="h-screen w-screen flex justify-center items-center">
+    <canvas width={900} height={900} className="border-2 border-black-200"/>
   </div>
 }
